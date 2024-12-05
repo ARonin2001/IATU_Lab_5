5ded32ddb8816f16a8ced8390fdc8ba33c3749a9
ARonin2001 <aronin.2001@gmail.com>
2024-12-05 23:28:18 +0400

    переработка задания

--- main.py
+++ main.py
@@ -1,47 +1,73 @@
-import math
 import time
+import math
 
-def F(n):
+def factorial(n):
+    if n == 0:
+        return 1
+    elif n < 0:
+        return "Факториал определен только для неотрицательных чисел"
+    else:
+        result = 1
+        for i in range(1, n + 1):
+            result *= i
+        return result
+
+def recursive_f(n):
 	if n < 3:
 		return 1
-	res = (-1)**n * (F(n - 2) / math.factorial(2 * n))
+	else:
+	    try:
+	        return (-1)**n * (recursive_f(n - 2) / factorial(2 * n))
+	    except ZeroDivisionError:
+	        return "Деление на ноль!"
 
-	return res
 
-def I(n):
-	default = 1
+def iterative_f(n):
+	if n < 1:
+		return "Недопустимое значение n!"
+
 	if n < 3:
-		return default
+		return 1
+	else:
+	    f_n_minus_2 = 1
+	    for i in range(3, n + 1):
+	        try:
+	            f_n = (-1) ** i * (f_n_minus_2 / factorial(2 * i))
+	            f_n_minus_2 = f_n
+	        except ZeroDivisionError:
+	            return "Деление на ноль!"
+	        except OverflowError:
+	            return "Переполнение!"
+	    return f_n_minus_2
 
-	array = [default, default]
-	res = 0
-	for i in range(3, n + 1):
-		calculated = (-1)**i * (array[i - 3] / math.factorial(2 * i))
-		array.append(calculated)
-		res = calculated
 
-	return res
+def compare_times(n_values):
+    results = []
+    for n in n_values:
+        recursive_start = time.time()
+        recursive_result = recursive_f(n)
+        recursive_end = time.time()
+
+        iterative_start = time.time()
+        iterative_result = iterative_f(n)
+        iterative_end = time.time()
+
+
+        results.append([n,
+                       recursive_end - recursive_start,
+                       iterative_end - iterative_start,
+                       recursive_result,
+                       iterative_result])
+    return results
 
 def main():
-	inputVal = 10
+	n_values = range(1, 10)
+	results = compare_times(n_values)
 
-	startTime = time.time()
-
-	res = F(inputVal)
-
-	endTime = time.time()
-
-	print("Result F:", res)
-	print("Time F:", endTime - startTime)
-
-	iStartTime = time.time()
-
-	res = I(inputVal)
-
-	iEndTime = time.time()
-
-	print("Result I:", res)
-	print("Time I:", iEndTime - iStartTime)
+	print("Результаты сравнения:")
+	print("n\tРекурсивное время\tИтерационное время\tРезультат рекурсивный\tРезультат итерационный")
+	for result in results:
+	    print(f"{result[0]}\t{result[1]:.6f}\t\t{result[2]:.6f}\t\t{result[3]}\t\t{result[4]}")
 
 if __name__ == "__main__":
 	main()
